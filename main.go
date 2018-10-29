008e42fb08510b3055c9758a45d956abe3a175e6
Cameron Whitehead <camjw119@gmail.com>
2018-10-29 22:48:58 +0000

    Using main to feature test still

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import board "github.com/camjw/tic-tac-go/lib"
+import "fmt"
 
 func main() {
   var board = board.Board{}
   board.Initialize()
-  board.PlayMove(0,0,"X")
+  board.PlayMove(0,"X")
+  board.PlayMove(3,"X")
+  board.PlayMove(6,"X")
   board.Print()
+  fmt.Println(board.VerticalWin("X"))
 }
